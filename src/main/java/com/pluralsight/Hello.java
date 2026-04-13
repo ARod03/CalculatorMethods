afa77a53f499e9425804138480497f78de403428
ARod03 <ardgz2003@gmail.com>
2026-04-13 14:40:00 -0500

    First Commit

--- src/main/java/com/pluralsight/Hello.java
+++ src/main/java/com/pluralsight/Hello.java
@@ -4,30 +4,26 @@ public class Hello {
 
     public static void main(String[] args) {
 
+        //Call out each method
         sayHello();
 
         sayGoodbye();
 
         sayGoodMorning();
 
-
-
     }
 
+    //Create all methods
     public static void sayHello() {
         System.out.println("Hello, World!");
-
     }
 
     public static void sayGoodbye() {
             System.out.println("Goodbye!");
-
     }
 
     public static void sayGoodMorning() {
         System.out.println("Good morning!");
-
-
     }
 
 }
